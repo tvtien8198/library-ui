3cce2311dfa57be5465d768c6b07b40d85eba817
Tiến Trần văn <you@example.com>
2021-07-29 18:08:46 +0700

    first commit

---
+++ reducer.js
@@ -0,0 +1,15 @@
+const init = {
+    cars: ['BMW']
+}
+export default function reducer(state = init, action, args) {
+    switch(action){
+        case 'ADD':
+            const [newCar] = args
+            return {
+                ...state,
+                cars: [...state.cars, newCar]
+            }
+        default:
+            return state
+    }
+}
